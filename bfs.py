a90f3dfefaf72732ecfa5948c6073ef5639382b6
KARIKEMANIDEEP <117148621+KARIKEMANIDEEP@users.noreply.github.com>
2022-11-01 12:07:25 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,25 @@
+graph = { 
+    'P' : ['Q', 'R', 'S'],
+    'Q' : ['P', 'R'],
+    'R' : ['P', 'Q', 'T'],
+    'T' : ['R'],
+    'S' : ['P']
+}
+
+visited = []
+queue = []
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+    
+    while queue:
+        m = queue.pop(0)
+        print(m, end=" ")
+        for neighbour in graph[m]:
+          if neighbour not in visited:
+            visited.append(neighbour)
+            queue.append(neighbour)
+
+print("Following is the Breadth-First search")
+bfs(visited, graph, 'P')
